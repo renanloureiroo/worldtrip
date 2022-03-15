6b2f86e17d9e6bc6be5855c1b46eb5e64aae8845
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-03-15 17:25:01 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Flex, Text, Box } from "@chakra-ui/react"
+import Image from "next/image"
+
+export const Header = () => {
+  return (
+    <Box as="header" w={"100%"}>
+      <Flex
+        w="100%"
+        align="center"
+        justify="center"
+        maxWidth={1440}
+        h="24"
+        mx="auto"
+      >
+        <Image src="/images/Logo.svg" width={180} height={45} alt="Logo" />
+      </Flex>
+    </Box>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react"
+import type { NextPage } from "next"
+import Head from "next/head"
+
+const Home: NextPage = () => {
+  return (
+    <Flex>
+      <Head>
+        <title>Worldtrip | Home </title>
+      </Head>
+    </Flex>
+  )
+}
+
+export default Home
